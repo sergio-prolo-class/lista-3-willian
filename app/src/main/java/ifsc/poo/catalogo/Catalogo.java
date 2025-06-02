347cf46ad2a17f9dbe5526aeb128eab097ad2657
Willian Ferreira dos Santos <willsantfer@gmail.com>
2025-06-01 21:48:08 -0300

    Edita a classe Catalogo

--- app/src/main/java/ifsc/poo/catalogo/Catalogo.java
+++ app/src/main/java/ifsc/poo/catalogo/Catalogo.java
@@ -1,9 +1,22 @@
 package ifsc.poo.catalogo;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Catalogo {
 
-    private Set<Filme> filmes = new 
+    private Set<Filme> filmes;
+
+    public Catalogo() {
+        filmes = new HashSet<>();
+    }
+
+    public boolean adicionarFilme(Filme filme) {
+        return filmes.add(filme);
+    }
+
+    public boolean removerFilme(String titulo) {
+        return filmes.removeIf(filme -> filme.getTitulo().equalsIgnoreCase(titulo));
+    }
 
 }
